666f5112808888aa4a9d6c18b953ae416b9eed82
itsjoacor <jdr123r@gmail.com>
2024-11-28 09:11:41 -0300

    Añado selector de tematica

--- web/src/pages/home/page.jsx
+++ web/src/pages/home/page.jsx
@@ -4,25 +4,52 @@ import { useNavigate } from "react-router-dom";
 
 function GamePlaySetUp() {
 	const [selectedSize, setSelectedSize] = useState(4);
+	const [selectedTheme, setSelectedTheme] = useState("Numbers"); // Default theme
 	const navigate = useNavigate();
 
 	const handleStart = () => {
-		navigate("/game", { state: { size: selectedSize } });
+		navigate("/game", { state: { size: selectedSize, theme: selectedTheme } });
 	};
 
 	return (
 		<div className="flex items-center justify-center h-screen w-screen text-white">
 			<div className="bg-black rounded-lg shadow-lg p-10 w-[90%] max-w-md flex flex-col items-center justify-center">
-				<h1 className="text-4xl font-bold mb-8 text-center">Memo Test</h1>
+				<h1 className="text-4xl font-bold mb-6 text-center">Memo Test</h1>
+
 
 				<BoardSize
 					selectedSize={selectedSize}
 					setSelectedSize={setSelectedSize}
 				/>
 
+
+				<div className="mt-4 flex space-x-4">
+					<button
+						onClick={() => setSelectedTheme("Opcion 1")}
+						className={`px-4 py-2 text-lg font-bold rounded ${
+							selectedTheme === "Numbers"
+								? "bg-blue-500 text-white"
+								: "bg-gray-700 text-gray-300 hover:bg-gray-600"
+						}`}
+					>
+						Opcion 1
+					</button>
+					<button
+						onClick={() => setSelectedTheme("Opcion 2")}
+						className={`px-4 py-2 text-lg font-bold rounded ${
+							selectedTheme === "Animals"
+								? "bg-blue-500 text-white"
+								: "bg-gray-700 text-gray-300 hover:bg-gray-600"
+						}`}
+					>
+						Opcion 2
+					</button>
+				</div>
+
+				{/* Start Button */}
 				<button
 					onClick={handleStart}
-					className="px-6 py-3 bg-green-500 text-white text-lg font-bold rounded hover:bg-green-600 w-full"
+					className="mt-6 px-6 py-3 bg-green-500 text-white text-lg font-bold rounded hover:bg-green-600 w-full"
 				>
 					Start
 				</button>
