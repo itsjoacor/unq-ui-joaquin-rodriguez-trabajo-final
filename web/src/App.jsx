65fe36f72f6cd33fcecc92e3ccb6d82e39faeb82
itsjoacor <jdr123r@gmail.com>
2024-11-28 13:16:56 -0300

    removed import app.css - running

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import GamePlaySetUp from "./pages/home/page"
 import Game from "./pages/game/page"
-import './App.css'
+
 
 function App() {
 	return (
