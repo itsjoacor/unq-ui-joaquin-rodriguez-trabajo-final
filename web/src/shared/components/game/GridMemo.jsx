f69182a09411b246ee427a258ad7bf7b43b7643b
itsjoacor <jdr123r@gmail.com>
2024-12-09 13:51:07 -0300

    style casi terminado

--- web/src/shared/components/game/GridMemo.jsx
+++ web/src/shared/components/game/GridMemo.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function GridMemo({ size, grid, selected, handleSquareClick }) {
 	return (
-		<div className="flex items-center justify-center">
+		<div className="flex items-center justify-center p-4">
 			<div
-				className={`grid gap-4 ${
+				className={`grid gap-2 ${
 					size === 4
 						? "grid-cols-4"
 						: size === 6
@@ -16,7 +16,11 @@ function GridMemo({ size, grid, selected, handleSquareClick }) {
 					<div
 						key={square.id}
 						className={`relative ${
-							size === 4 ? "h-24 w-24" : size === 6 ? "h-20 w-20" : "h-16 w-16"
+							size === 4
+								? "h-16 w-16 sm:h-20 sm:w-20 md:h-24 md:w-24"
+								: size === 6
+								? "h-14 w-14 sm:h-18 sm:w-18 md:h-20 md:w-20"
+								: "h-12 w-12 sm:h-16 sm:w-16 md:h-18 md:w-18"
 						} cursor-pointer perspective`}
 						onClick={() => handleSquareClick(index)}
 					>
@@ -25,17 +29,18 @@ function GridMemo({ size, grid, selected, handleSquareClick }) {
 								selected.includes(index) || square.matched ? "rotate-y-180" : ""
 							}`}
 						>
-							<div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-indigo-600 to-blue-500 text-white text-5xl font-bold rounded-xl shadow-lg backface-hidden">
+							<div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-indigo-600 to-blue-500 text-white text-3xl sm:text-4xl md:text-5xl font-bold rounded-xl shadow-lg backface-hidden">
 								?
 							</div>
-
 							<div
 								className={`absolute inset-0 flex items-center justify-center ${
-									square.matched ? "bg-green-500" : "bg-rose-600"
-								} border-4 border-transparent bg-gradient-to-r text-black text-2xl font-bold rounded-lg transform rotate-y-180 backface-hidden`}
+									square.matched ? "bg-green-500" : "bg-yellow-400"
+								} border-4 border-transparent text-black text-xl sm:text-2xl md:text-3xl font-bold rounded-lg transform rotate-y-180 backface-hidden`}
 							>
 								<div className="bg-white rounded-lg flex items-center justify-center w-3/4 h-3/4">
-									<span className="text-3xl font-bold">{square.value}</span>
+									<span className="text-2xl sm:text-3xl md:text-4xl font-bold">
+										{square.value}
+									</span>
 								</div>
 							</div>
 						</div>
