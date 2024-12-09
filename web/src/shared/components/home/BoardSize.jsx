63dbe4dcb14b7aab47d5c85fa6ec7d46e335973c
itsjoacor <jdr123r@gmail.com>
2024-12-09 12:17:49 -0300

    gradient button

--- web/src/shared/components/home/BoardSize.jsx
+++ web/src/shared/components/home/BoardSize.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function BoardSize({ selectedSize, setSelectedSize }) {
 	return (
-		<div className="flex flex-col items-center justify-center h-full mb-6">
+		<div className="flex flex-col items-center justify-center h-full">
 			<h2 className="text-2xl font-semibold mb-4 text-red-500">
 				Choose Grid Size
 			</h2>
@@ -13,8 +13,8 @@ function BoardSize({ selectedSize, setSelectedSize }) {
 						onClick={() => setSelectedSize(size)}
 						className={`w-16 h-16 rounded-full flex items-center justify-center text-2xl font-bold ${
 							selectedSize === size
-								? "bg-blue-500 text-white"
-								: "bg-gray-700 text-gray-300 hover:bg-gray-600"
+								? "bg-gradient-to-r from-indigo-500 to-blue-500 text-white"
+								: "bg-gradient-to-r from-gray-700 to-gray-500 text-gray-300 hover:from-gray-600 hover:to-gray-500"
 						}`}
 					>
 						{size}x{size}
