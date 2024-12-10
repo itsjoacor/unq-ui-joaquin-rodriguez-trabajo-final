09e5a0d38cc8db38b069a562eccda24329a01b54
itsjoacor <jdr123r@gmail.com>
2024-12-10 13:03:06 -0300

    Fix homePage

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -22,43 +22,47 @@ function GamePlaySetUp() {
 	};
 
 	return (
-		<div className="relative flex items-center justify-center min-h-screen min-w-screen bg-black text-white">
+		<div className="relative flex items-center justify-center h-screen w-screen bg-black text-white">
 			<button
 				onClick={handleInstructions}
-				className="absolute top-4 right-4 text-white text-4xl hover:text-yellow-200"
+				className="absolute top-4 right-4 text-white text-3xl md:text-4xl hover:text-yellow-200"
 				title="Instructions"
 			>
 				<FiInfo />
 			</button>
 
 			<div
-				className="bg-black rounded-lg shadow-lg p-10 w-[90%] max-w-md flex flex-col items-center justify-center
-      bg-gradient-to-r from-purple-500 to-fuchsia-400 bg-clip-text text-transparent"
+				className="bg-black rounded-lg shadow-lg px-6 py-8 w-full max-w-md flex flex-col items-center justify-center
+          bg-gradient-to-r from-purple-500 to-fuchsia-400 bg-clip-text text-transparent"
 			>
-				<h1 className="text-6xl font-bold mb-6 text-center pb-6">Memo Test</h1>
+				<h1 className="text-5xl md:text-6xl font-bold mb-6 text-center">
+					Memo Test
+				</h1>
 
-				<BoardSize
-					selectedSize={selectedSize}
-					setSelectedSize={setSelectedSize}
-				/>
+				<div className="w-full flex flex-col items-center gap-6">
+					<BoardSize
+						selectedSize={selectedSize}
+						setSelectedSize={setSelectedSize}
+					/>
 
-				<ThemeSelector
-					selectedTheme={selectedTheme}
-					setSelectedTheme={setSelectedTheme}
-				/>
+					<ThemeSelector
+						selectedTheme={selectedTheme}
+						setSelectedTheme={setSelectedTheme}
+					/>
 
-				<PlayersSetUp
-					multiplayer={multiplayer}
-					setMultiplayer={setMultiplayer}
-				/>
+					<PlayersSetUp
+						multiplayer={multiplayer}
+						setMultiplayer={setMultiplayer}
+					/>
 
-				<button
-					onClick={handleStart}
-					className="mt-6 px-6 py-3 text-white text-2xl font-bold rounded w-1/3
-          bg-gradient-to-r from-emerald-500 to-lime-400 hover:from-green-500 hover:to-green-500"
-				>
-					Start
-				</button>
+					<button
+						onClick={handleStart}
+						className="mt-6 px-6 py-3 text-white text-xl md:text-2xl font-bold rounded w-2/3 md:w-1/3
+              bg-gradient-to-r from-emerald-500 to-lime-400 hover:from-green-500 hover:to-green-500"
+					>
+						Start
+					</button>
+				</div>
 			</div>
 		</div>
 	);
